61f28e8647eb67d1ab0ea48749f4cc282449901f
misrori <ormraat.pte@gmail.com>
2018-09-18 12:46:49 +0200

    init

---
+++ 1.R
@@ -0,0 +1,8 @@
+#why are we here
+#git explained
+#create R project
+#create and pull repo with R and online
+#function expalined and create some
+#reading data and explain the structure
+#rename, add column, names, rownames, basic filter
+
